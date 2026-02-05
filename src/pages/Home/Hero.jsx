9fcd21c9c182809805424821811870d150038321
Andreeagaldea <andreeagaldea@gmail.com>
2026-02-05 17:48:29 +0200

    wip hero - added parallax

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useRef } from 'react';
+import Parallax from 'parallax-js';
 import './Home.css'
 
 function Hero() {
+const sceneRef = useRef(null);
+
+  useEffect(() => {
+    let parallaxInstance;
+    if (sceneRef.current) {
+      parallaxInstance = new Parallax(sceneRef.current, {
+        relativeInput: true,
+        hoverOnly: true,
+        selector: '.layer' 
+      });
+    }
+
+    return () => {
+      parallaxInstance?.destroy();
+    }
+  }, []);
 
   return (
-    <div id="hero" className="background background-filter bg-cover bg-center flex items-center justify-center 
-                min-h-[calc(100vh-var(--nav-height,0px))] ">
+    <div id="hero" ref={sceneRef} 
+                className="background background-filter bg-cover bg-center flex items-center justify-center 
+                min-h-[calc(100vh-var(--nav-height,0px))] overflow-hidden">
       <div className="content flex flex-col lg:flex-row w-full items-center justify-center max-w-7xl">
+
         <div className='w-full lg:w-1/2 p-5 flex flex-col justify-center'>
           <h1 className="text-white text-6xl font-bold pb-5 text-center lg:text-left">
             Pure <span className='special-text'>Artistry</span> In Every Bean.</h1>
@@ -13,19 +33,28 @@ function Hero() {
             Redefining the standard of premium coffee. Experience a curated 
             journey from the worlds's moste exclusive high-altitude estates to your morning ritual.</p>
         </div>
-        <div className='w-full lg:w-1/2 flex justify-center lg:justify-end p-5 relative'>
-          <img src="../../src/assets/coffee-bag.png" alt="" className='' style={{maxHeight:'550px', zIndex:10}}/>
-          <div className='beans'>
-            <img src="../../src/assets/beans/1.png" alt="" className='absolute' 
-              style={{zIndex:11, right:0, bottom:'-7%'}}/>
-            {/* <img src="../../src/assets/beans/2.png" alt="" className='absolute' 
-              style={{zIndex:11, right:0, top:0, }}/> */}
-            <img src="../../src/assets/beans/3.png" alt="" className='absolute' 
-              style={{zIndex:11, right:'7%', top: '2%', }}/>
-            <img src="../../src/assets/beans/4.png" alt="" className='absolute' 
-              style={{zIndex:1, right:0, left:'15%', bottom:0, maxHeight: '380px', filter: 'blur(1px)'}}/>
+
+        <div className='w-full lg:w-1/2 min-h-[550px] relative'>
+          
+          <div className="layer absolute inset-0 w-full h-full" data-depth="0.1" style={{ zIndex: 1 }}>
+            <img src="../../src/assets/beans/4.png" className="absolute opacity-60" 
+                 style={{ left: '10%', top: '10%', maxHeight: '400px', filter: 'blur(4px)' }} />
           </div>
+
+          <div className="layer absolute inset-0 w-full h-full flex justify-center lg:justify-end" data-depth="0.2" style={{ zIndex: 5 }}>
+            <img src="../../src/assets/coffee-bag.png" alt="Coffee Bag" style={{ maxHeight: '550px' }} />
           </div>
+
+          <div className="layer absolute inset-0 w-full h-full" data-depth="0.4" style={{ zIndex: 10 }}>
+            <img src="../../src/assets/beans/3.png" className="absolute" style={{ right: '25%', top: '5%' }} />
+          </div>
+
+          <div className="layer absolute inset-0 w-full h-full" data-depth="0.6" style={{ zIndex: 11 }}>
+            <img src="../../src/assets/beans/1.png" className="absolute" style={{ right: '5%', bottom: '5%' }} />
+          </div>
+
+        </div>
+
       </div>
     </div>
 
